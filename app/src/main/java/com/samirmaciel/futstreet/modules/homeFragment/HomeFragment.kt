b6e4b4c9ee9f63fcf6544125389d16ec4eac55c4
samirmaciel <samirmaciel.dev@gmail.com>
2021-09-20 19:16:12 -0300

    Main navGraph created

---
+++ app/src/main/java/com/samirmaciel/futstreet/modules/homeFragment/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.samirmaciel.futstreet.modules.homeFragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.samirmaciel.futstreet.R
+import com.samirmaciel.futstreet.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var _binding : FragmentHomeBinding? = null
+    private val binding : FragmentHomeBinding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentHomeBinding.bind(view)
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
